49127fcec2f0dd28ddbd8826704deb0554cbe2bc
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-09-08 16:48:33 +0700

    Implemented the pattern

---
+++ ConsoleApp1/ConsoleApp1/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+        private Singleton()
+        {
+        }
+    }
+}
+
+//Был выбрал именно этот способ реализации из соображений потокобезопасности и отложенной реализации.
+//Оба свойства обеспечиваются использованием типа Lazy<> (по умолчанию объекты Lazy<> потокобезопасны), о котором прочитал тут:
+//https://docs.microsoft.com/en-us/dotnet/framework/performance/lazy-initialization
+
